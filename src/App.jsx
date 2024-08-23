d2c943df6f70207c30d8a9471d40f4fafb088fea
nitin telang <nitintelang110@gmail.com>
2024-08-23 21:21:33 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const [show, setShow] = useState(true);
+  const [txtColor, setTxtColor] = useState('');
+  const [color, setColor] = useState({
+    name:''
+  })
+  
+  const handleClick = () => {
+    setShow(!show)
+  }
+
+  const handleColor = (e) => {
+     setColor({
+            ...color,
+            [e.target.name]: e.target.value
+        })
+  }
+
+ 
+  
+  const handleChangeColor = () => {
+  setTxtColor(color.name)
+  }
+  return (
+    <>
+       <input
+                    type="text"
+                    name="name"
+                    value={color.name}
+                    onChange={handleColor}
+                    className="form-control"
+                    
+      />
+      <br /><br /><br />
+      
+      {show ? <h1 className="namee" style={{color:txtColor}}>A.R. ENGINEERINGS</h1>:""}
+      <button className='btn' onClick={handleClick}>SHOW NAME</button> 
+      <button className='btn' onClick={handleChangeColor}>Change Color</button> 
+    </>
+  )
+}
+
+export default App;
